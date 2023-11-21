7b5867e7dbaae8aaad3f8989b939d75c9ff9f68d
romys <romysaputrasihanandaa@gmail.com>
2023-11-21 22:03:17 +0700

    first commit

---
+++ src/main/java/com/romys/strorewithjersey/StrorewithjerseyApplication.java
@@ -0,0 +1,13 @@
+package com.romys.strorewithjersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrorewithjerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrorewithjerseyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romys/strorewithjersey/StrorewithjerseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romys.strorewithjersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrorewithjerseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
