1186150782ea067c2848c59e51668c3d3aa30a46
romys <romysaputrasihanandaa@gmail.com>
2023-11-22 16:35:44 +0700

    update

--- src/main/java/com/romys/services/ProductService.java
+++ src/main/java/com/romys/services/ProductService.java
@@ -3,6 +3,7 @@ package com.romys.services;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,22 +11,27 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.client.WebResource;
 
 @Service
 public class ProductService {
-    @Autowired
-    private WebResource webResource;
+    @Value("${service.elastic.host}")
+    private String host;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private Client client;
 
     public JsonNode getAll()
             throws JsonMappingException, JsonProcessingException, ClientHandlerException, UniformInterfaceException {
         return this.objectMapper.readValue(
-                this.webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class).getEntity(String.class),
+                this.client.resource(host).accept(MediaType.APPLICATION_JSON).get(ClientResponse.class)
+                        .getEntity(String.class),
                 JsonNode.class).get("_source");
     }
 }
