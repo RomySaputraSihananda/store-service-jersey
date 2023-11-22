10e6fe0f29351515e482248cb8f85c55b4c75551
romys <romysaputrasihanandaa@gmail.com>
2023-11-23 00:24:53 +0700

    update

--- src/main/java/com/romys/controllers/ProductController.java
+++ src/main/java/com/romys/controllers/ProductController.java
@@ -1,7 +1,8 @@
 package com.romys.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,18 +14,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.Client;
+
 import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.UniformInterfaceException;
+
 import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-
 import com.romys.models.ProductModel;
+import com.romys.payloads.responses.BodyResponse;
+import com.romys.payloads.responses.BodyResponses;
 import com.romys.services.ProductService;
 
 @RestController
@@ -33,22 +33,11 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @Value("${service.elastic.host}")
-    private String host;
-
-    @Value("${service.elastic.index.product}")
-    private String index;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private Client client;
-
     @GetMapping
-    public List<?> getAllProducts()
+    public ResponseEntity<BodyResponses<List<ProductModel>>> getAllProducts()
             throws JsonMappingException, JsonProcessingException, ClientHandlerException, UniformInterfaceException {
-        return this.service.getAll();
+        return new ResponseEntity<>(new BodyResponses<>(HttpStatus.OK.getReasonPhrase(), HttpStatus.OK.value(),
+                "all data products", this.service.getAll()), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
@@ -81,16 +70,5 @@ public class ProductController {
     public List<ProductModel> searchByField(@RequestParam String field, @RequestParam String value)
             throws JsonMappingException, JsonProcessingException, ClientHandlerException, UniformInterfaceException {
         return this.service.getByField(field, value);
-    }
-
-    @GetMapping("/test")
-    public List<?> getMethodName()
-            throws JsonMappingException, JsonProcessingException, UniformInterfaceException, ClientHandlerException {
-        return this.objectMapper.readValue(this.objectMapper.readValue(
-                this.client.resource(String.format("%s/%s/_search", host, index)).accept(MediaType.APPLICATION_JSON)
-                        .get(String.class),
-                JsonNode.class).get("hits").get("hits").toString(),
-                new TypeReference<List<?>>() {
-                });
     }
 }
